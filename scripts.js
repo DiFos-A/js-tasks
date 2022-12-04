2bdd729744dd07d3d1cd98ce0ff3be2455261207
DiFos <98109325+DiFos-A@users.noreply.github.com>
2022-12-04 19:04:38 +0300

    js6

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let string = "nissan gt r-32";
-// function ucFirst(str) {
-//     let newStr = str[0].toUpperCase() + str.slice(1)
-//     console.log(newStr);
-// }
-// ucFirst(string);
+let string = "nissan gt r-32";
+function ucFirst(str) {
+    let newStr = str[0].toUpperCase() + str.slice(1)
+    console.log(newStr);
+}
+ucFirst(string);
 
 //-----------------------------------------------------------------------------------------------
 
@@ -19,13 +19,13 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let strIng = "XXX new carcenter XXX";
-// function checkSpam(str) {
-//     let newSTr = str.toLowerCase();
-//     let result = (newSTr.includes("badWord".toLowerCase()) || newSTr.includes("XXX".toLowerCase())) ? true : false;
-//     return result;
-// }
-// console.log(checkSpam(strIng));
+let strIng = "XXX new carcenter XXX";
+function checkSpam(str) {
+    let newSTr = str.toLowerCase();
+    let result = (newSTr.includes("badWord".toLowerCase()) || newSTr.includes("XXX".toLowerCase())) ? true : false;
+    return result;
+}
+console.log(checkSpam(strIng));
 
 
 //-----------------------------------------------------------------------------------------------
@@ -36,12 +36,12 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let string = "привет Женя";
-// function checkSpam(str) {
-//     str = str.split("").reverse().join("");
-//     return str;
-// }
-// console.log(checkSpam(string));
+let string = "привет Женя";
+function checkSpam(str) {
+    str = str.split("").reverse().join("");
+    return str;
+}
+console.log(checkSpam(string));
 
 //-----------------------------------------------------------------------------------------------
 
@@ -68,18 +68,18 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let stations = [
-//     'MAN675847583748sjt567654;Manchester Piccadilly',
-//     'GNF576746573fhdg4737dh4;Greenfield',
-//     'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
-//     'SYB4f65hf75f736463;Stalybridge',
-//     'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'
-// ];
-// for (i = 0; i < stations.length; i++) {
-//     let slicedPart = stations[i].slice(0, 3);
-//     let slicedName = stations[i].slice((stations[i].indexOf(";") + 1));
-//     console.log(slicedPart + ': ' + slicedName);
-// }
+let stations = [
+    'MAN675847583748sjt567654;Manchester Piccadilly',
+    'GNF576746573fhdg4737dh4;Greenfield',
+    'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
+    'SYB4f65hf75f736463;Stalybridge',
+    'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'
+];
+for (i = 0; i < stations.length; i++) {
+    let slicedPart = stations[i].slice(0, 3);
+    let slicedName = stations[i].slice((stations[i].indexOf(";") + 1));
+    console.log(slicedPart + ': ' + slicedName);
+}
 
 //-----------------------------------------------------------------------------------------------
 
@@ -89,21 +89,21 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let mass = [12, 4, 50, 1, 0, 18, 40];
-// let arr = [56, 24, -55, 2, 9, -345];
+let mass = [12, 4, 50, 1, 0, 18, 40];
+let arr = [56, 24, -55, 2, 9, -345];
 
-// function findZero(arr){
-//      if(arr.find(item => item == 0)){
-//         return true;
-//      } 
-//      else{
-//         return false;
-//      }
+function findZero(arr){
+     if(arr.find(item => item == 0)){
+        return true;
+     } 
+     else{
+        return false;
+     }
     
 
-// }
+}
 
-// console.log(findZero(mass));
+console.log(findZero(mass));
 
 //-----------------------------------------------------------------------------------------------
 
@@ -119,17 +119,17 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let strings = ["кришна", "кришна", "харе", "харе", "харе", "харе", "кришна", "кришна", ":-O"];
-// function unique(arr) {
-//     let newStr = [];
-//     for (i = 0; i < arr.length; i++) {
-//         if (!(newStr.includes(arr[i]))) {
-//             newStr.push(arr[i]);
-//         }
-//     }
-//     return newStr;
-// }
-// console.log(unique(strings))
+let strings = ["кришна", "кришна", "харе", "харе", "харе", "харе", "кришна", "кришна", ":-O"];
+function unique(arr) {
+    let newStr = [];
+    for (i = 0; i < arr.length; i++) {
+        if (!(newStr.includes(arr[i]))) {
+            newStr.push(arr[i]);
+        }
+    }
+    return newStr;
+}
+console.log(unique(strings))
 
 
 //-----------------------------------------------------------------------------------------------
@@ -158,24 +158,25 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// catsJane = [4, 5, 3, 11, 6, 2, 4, 1, 5, 9]
-// catsJulia = [2, 4, 5, 1, 13, 2, 15, 8, 3, 7]
-// catsJanes = [3, 5, 9, 14, 1, 2, 6, 8, 3, 10]
-// catsJulias = [8, 2, 10, 1, 2, 5, 6, 3, 1, 4]
-// function verifyCats(catsJane, catsJulia) {
-//     let newcatsJane =  catsJane.splice(1, catsJane.length - 2);
-//     let common = newcatsJane.concat(catsJulia);
-//     common.forEach(function(item, i, common) {
-//         if(item > 6){
-//             console.log("Кошка №" + i + " взрослая, ей " + item + " лет.");
-//         }
-//         else{
-//             console.log("Кошка №" + i + " ещё котёнок, ему " + item + " лет.");
-//         }
-//     });
-// }
-// console.log(verifyCats(catsJane, catsJulia));
-// console.log(verifyCats(catsJanes, catsJulias));
+catsJane = [4, 5, 3, 11, 6, 2, 4, 1, 5, 9]
+catsJulia = [2, 4, 5, 1, 13, 2, 15, 8, 3, 7]
+catsJanes = [3, 5, 9, 14, 1, 2, 6, 8, 3, 10]
+catsJulias = [8, 2, 10, 1, 2, 5, 6, 3, 1, 4]
+function verifyCats(catsJane, catsJulia) {
+    let newcatsJane =  catsJane.splice(1, catsJane.length - 2);
+    let common = newcatsJane.concat(catsJulia);
+    common.forEach(function(item, i, common) {
+        if(item > 6){
+            console.log("Кошка №" + i + " взрослая, ей " + item + " лет.");
+        }
+        else{
+            console.log("Кошка №" + i + " ещё котёнок, ему " + item + " лет.");
+        }
+    });
+}
+console.log(verifyCats(catsJane, catsJulia));
+console.log(verifyCats(catsJanes, catsJulias));
+
 //-----------------------------------------------------------------------------------------------
 
 // Задание 8
@@ -189,14 +190,14 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let arr = [5, 4, 3, 8, 0];
-// function filterFor(arr, a) {
-//     let newArr = arr.filter(function (item) {
-//         return item > a;
-//     })
-//     return newArr;
-// }
-// console.log(filterFor(arr, 3))
+let arr = [5, 4, 3, 8, 0];
+function filterFor(arr, a) {
+    let newArr = arr.filter(function (item) {
+        return item > a;
+    })
+    return newArr;
+}
+console.log(filterFor(arr, 3))
 
 //-----------------------------------------------------------------------------------------------
 
@@ -207,13 +208,13 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let strings = ['yes', 'hello', 'no', 'easycode', 'what'];
-// function sortWord(arr){
-//     let mewStrings = arr.filter(item => item.length > 3);
-//     return mewStrings;
-// }
+let strings = ['yes', 'hello', 'no', 'easycode', 'what'];
+function sortWord(arr){
+    let mewStrings = arr.filter(item => item.length > 3);
+    return mewStrings;
+}
 
-// console.log(sortWord(strings));
+console.log(sortWord(strings));
 
 
 //-----------------------------------------------------------------------------------------------
@@ -227,16 +228,16 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let arr = [ [14, 45], [1], ['a', 'c', 'd'] ];
-// let mas = [ [1], [14, 45], ['a', 'c', 'd'] ];
+let arr = [ [14, 45], [1], ['a', 'c', 'd'] ];
+let mas = [ [1], [14, 45], ['a', 'c', 'd'] ];
 
-// function sortMin(arr){
-//     let mass = arr.sort((a, b) => a.length - b.length);
-//     return mass;
-// }
+function sortMin(arr){
+    let mass = arr.sort((a, b) => a.length - b.length);
+    return mass;
+}
 
-// console.log(sortMin(arr));
-// console.log(sortMin(mas));
+console.log(sortMin(arr));
+console.log(sortMin(mas));
 
 //-----------------------------------------------------------------------------------------------
 
@@ -256,26 +257,26 @@
 
 //-----------------------------------------------------------------------------------------------
 
-// let catAges = [7, 3, 2, 4, 1, 15, 8, 1, 9, 2];
-// let catAges2 = [1, 16, 12, 4, 5, 1, 3, 11, 7, 2];
-// function getAverageHumanAge(arr) {
-//     let humanAge = arr.map(function (item) {
-//         if(item <= 2){
-//             item *= 10;
-//         } else{
-//             item *= 7;
-//         }  
-//         return item; 
-//     });
+let catAges = [7, 3, 2, 4, 1, 15, 8, 1, 9, 2];
+let catAges2 = [1, 16, 12, 4, 5, 1, 3, 11, 7, 2];
+function getAverageHumanAge(arr) {
+    let humanAge = arr.map(function (item) {
+        if(item <= 2){
+            item *= 10;
+        } else{
+            item *= 7;
+        }  
+        return item; 
+    });
 
-//     let adultAge = humanAge.filter((item) => item > 18);
+    let adultAge = humanAge.filter((item) => item > 18);
 
-//     const sum = adultAge.reduce((acc, item) => acc += item, 0);
+    const sum = adultAge.reduce((acc, item) => acc += item, 0);
 
-//     return avg = sum / adultAge.length;
-// }
+    return avg = sum / adultAge.length;
+}
 
-// console.log(getAverageHumanAge(catAges));
-// console.log(getAverageHumanAge(catAges2));
+console.log(getAverageHumanAge(catAges));
+console.log(getAverageHumanAge(catAges2));
 
 //-----------------------------------------------------------------------------------------------
